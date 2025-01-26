d6d4abacd45a41757d60f1ed22d9bdee56ffd1d7
enarcisse <en@infoport.re>
2025-01-26 20:10:16 +0400

    First commit

---
+++ app.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer/>
+    <div>
+      <div
+          data-animation="default"
+          class="nav w-nav" data-easing2="ease" data-easing="ease" data-collapse="medium"
+          data-w-id="c45a2868-bedb-50c4-d771-83c7c42577fb" role="banner" data-duration="400" data-element="nav"
+          style="background-color: rgba(0, 0, 0, 0); opacity: 1; visibility: inherit;">
+        <div class="padding-global is-relative">
+          <div class="nav_container">
+            <div class="nav_content"><a href="/" aria-current="page" class="nav_logo w-nav-brand w--current"
+                                        aria-label="home"><img
+                src="https://cdn.prod.website-files.com/655f1188cf5e283d9d642d99/65782e12af1d447a19a36eac_fenriz-logo.svg"
+                loading="lazy" alt=""></a>
+              <nav role="navigation" class="nav_menu w-nav-menu"><a href="https://help.fenriz-gym.com/" target="_blank"
+                                                                    class="nav_link w-inline-block">
+                <div>Hilfe</div>
+                <div class="nav_link-underline"
+                     style="transform: translate3d(0px, 0px, 0px) scale3d(0, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;"></div>
+              </a><a href="/kurse" class="nav_link w-inline-block">
+                <div>KURSE</div>
+                <div class="nav_link-underline"
+                     style="transform: translate3d(0px, 0px, 0px) scale3d(0, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;"></div>
+              </a><a href="/events" class="nav_link w-inline-block">
+                <div>EVENTS</div>
+                <div class="nav_link-underline"
+                     style="transform: translate3d(0px, 0px, 0px) scale3d(0, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;"></div>
+              </a><a href="/preise" class="nav_link w-inline-block">
+                <div>PREISE</div>
+                <div class="nav_link-underline"
+                     style="transform: translate3d(0px, 0px, 0px) scale3d(0, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;"></div>
+              </a><a href="/ueber-uns" class="nav_link w-inline-block">
+                <div>Über uns</div>
+                <div class="nav_link-underline"
+                     style="transform: translate3d(0px, 0px, 0px) scale3d(0, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;"></div>
+              </a><a href="/kursplan" class="nav_link w-inline-block">
+                <div>KURSPLAN</div>
+                <div class="nav_link-underline"
+                     style="transform: translate3d(0px, 0px, 0px) scale3d(0, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;"></div>
+              </a><a href="https://shop.fenriz-gym.com" target="_blank" class="nav_link w-inline-block">
+                <div>SHOP</div>
+                <div class="nav_link-underline"
+                     style="transform: translate3d(0px, 0px, 0px) scale3d(0, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;"></div>
+              </a><a href="https://www.mysports.com/studio/fenriz-gym/login-register" target="_blank"
+                     class="nav_link w-inline-block">
+                <div>MITGLIEDERBEREICH</div>
+                <div class="nav_link-underline"
+                     style="transform: translate3d(0px, 0px, 0px) scale3d(0, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;"></div>
+              </a>
+                <div class="nav_language-switcher">
+                  <div class="w-locales-list">
+                    <div role="list" class="loc_list w-locales-items">
+                      <div role="listitem" class="loc_item w-locales-item"><a hreflang="de" href="/" aria-current="page"
+                                                                              class="loc_link w--current">Deutsch</a>
+                      </div>
+                      <div role="listitem" class="loc_item w-locales-item"><a hreflang="en" href="/en" class="loc_link">English</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </nav>
+              <div class="nav_menu-button w-nav-button" style="-webkit-user-select: text;" aria-label="menu"
+                   role="button" tabindex="0" aria-controls="w-nav-overlay-0" aria-haspopup="menu"
+                   aria-expanded="false">
+                <div data-is-ix2-target="1" class="nav_lottie" data-w-id="b0fb524a-27f1-41b1-c4bf-7d30030d8453"
+                     data-animation-type="lottie"
+                     data-src="https://cdn.prod.website-files.com/60c20f59db52d02daf92d05e/64873348e6c73b647d866a3a_burger-menu.json"
+                     data-loop="0" data-direction="1" data-autoplay="0" data-renderer="svg" data-default-duration="3"
+                     data-duration="0" data-ix2-initial-state="0">
+                  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                       viewBox="0 0 400 400" width="400" height="400" preserveAspectRatio="xMidYMid meet"
+                       style="width: 100%; height: 100%; transform: translate3d(0px, 0px, 0px); content-visibility: visible;">
+                    <defs>
+                      <clipPath id="__lottie_element_2">
+                        <rect width="400" height="400" x="0" y="0"></rect>
+                      </clipPath>
+                    </defs>
+                    <g clip-path="url(#__lottie_element_2)">
+                      <g id="burger_3" transform="matrix(1,0,0,1,200,199.875)" opacity="1" style="display: block;">
+                        <g class="barre_3" opacity="1" transform="matrix(1,0,0,1,0,75)">
+                          <path fill="rgb(29,29,29)" fill-opacity="1"
+                                d=" M110,0 C110,0 110,0 110,0 C110,5.519000053405762 105.51899719238281,10 100,10 C100,10 -100,10 -100,10 C-105.51899719238281,10 -110,5.519000053405762 -110,0 C-110,0 -110,0 -110,0 C-110,-5.519000053405762 -105.51899719238281,-10 -100,-10 C-100,-10 100,-10 100,-10 C105.51899719238281,-10 110,-5.519000053405762 110,0z"></path>
+                        </g>
+                      </g>
+                      <g id="burger_2" transform="matrix(1,0,0,1,200,199.875)" opacity="1" style="display: block;">
+                        <g class="barre_2" opacity="1" transform="matrix(1,0,0,1,0,0)">
+                          <path fill="rgb(29,29,29)" fill-opacity="1"
+                                d=" M110,0 C110,0 110,0 110,0 C110,5.519000053405762 105.51899719238281,10 100,10 C100,10 -100,10 -100,10 C-105.51899719238281,10 -110,5.519000053405762 -110,0 C-110,0 -110,0 -110,0 C-110,-5.519000053405762 -105.51899719238281,-10 -100,-10 C-100,-10 100,-10 100,-10 C105.51899719238281,-10 110,-5.519000053405762 110,0z"></path>
+                        </g>
+                      </g>
+                      <g id="burger_1" transform="matrix(1,0,0,1,200,199.875)" opacity="1" style="display: block;">
+                        <g class="barre_1" opacity="1" transform="matrix(1,0,0,1,0,-75)">
+                          <path fill="rgb(29,29,29)" fill-opacity="1"
+                                d=" M110,0 C110,0 110,0 110,0 C110,5.519000053405762 105.51899719238281,10 100,10 C100,10 -100,10 -100,10 C-105.51899719238281,10 -110,5.519000053405762 -110,0 C-110,0 -110,0 -110,0 C-110,-5.519000053405762 -105.51899719238281,-10 -100,-10 C-100,-10 100,-10 100,-10 C105.51899719238281,-10 110,-5.519000053405762 110,0z"></path>
+                        </g>
+                      </g>
+                    </g>
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="nav_backdrop-filter" style="will-change: opacity; opacity: 0;"></div>
+        <div class="w-nav-overlay" data-wf-ignore="" id="w-nav-overlay-0"></div>
+      </div>
+    </div>
+  </div>
+</template>
